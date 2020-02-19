9d0b7898817c0a2e1710075a27c041a77764010b
sergio <sergiog.mbg@gmail.com>
2020-02-19 16:46:58 +0100

    Cambios en ListarSuscripciones.
    Creadas funciones:
    - recorreSuscripciones()
    - calcularDias()
    - calcularBarra()
    
    Aún con errores, falta revisar y cambiar las funciones.

--- frontend/src/app/components/lista-suscripciones/lista-suscripciones.component.ts
+++ frontend/src/app/components/lista-suscripciones/lista-suscripciones.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ServicioSuscripcionesService } from 'src/app/services/servicio-suscripciones.service';
 import { Suscripcion } from 'src/app/modelo/Suscripcion';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-lista-suscripciones',
@@ -10,22 +11,45 @@ import { Suscripcion } from 'src/app/modelo/Suscripcion';
 export class ListaSuscripcionesComponent implements OnInit {
 
   public suscripciones: Suscripcion;
+  public now = new Date();
 
-  constructor(private servicioSuscripciones: ServicioSuscripcionesService) { }
+  constructor(private servicioSuscripciones: ServicioSuscripcionesService) {
+    $(document).ready(() => {
+      this.recorreSuscripciones();
+    });
+  }
 
   ngOnInit() {
     this.servicioSuscripciones.getSuscripciones().subscribe(
       res => {
         this.suscripciones = res;
-        this.prueba();
       },
       err => {
         console.log(err);
       }
-    ); 
+    );
   }
 
-  prueba() {
-    //$('.barraCarga')
+  // Recorre las suscripciones y pasa FechaExpira a calcularDias()
+  recorreSuscripciones() {
+    for (let i = 0; i < Object.keys(this.suscripciones).length; i++) {
+      console.log(this.suscripciones[i]);
+      this.calcularDias(this.suscripciones[i].expira);
+    }
   }
+
+  // Calcula los días entre la fecha actual y FechaExpira
+  calcularDias(fecha) {
+    const fechaSusc = new Date(fecha); // Hay que pasar el parámetro a Date
+    const dias = (fechaSusc.getTime() - this.now.getTime()) / (1000 * 3600 * 24);
+    console.log(Math.round(dias));
+    this.calcularBarra(dias);
+  }
+
+  calcularBarra(dias) {
+    const hoy = this.now.getDate();
+    const porcentaje = (hoy * 100) / dias;
+    $('#1').width(porcentaje + '%');
+  }
+
 }

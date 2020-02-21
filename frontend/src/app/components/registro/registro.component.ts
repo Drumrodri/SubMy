ac157f115b1b6cea5cabd8b7f8b0ee2bfdfd47c4
Drumrodri <rodrigo.airen.0910@gmail.com>
2020-02-21 20:32:07 +0100

    validacion del formulario de registro
    Frontend
    se ha hecho en el ts y html de componente registro las operaciones pertinentes
    para que se valide el
    correo en el input correspondiente, falta
    mas validadores con expresiones regulares.

--- frontend/src/app/components/registro/registro.component.ts
+++ frontend/src/app/components/registro/registro.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ModeloUsuarioService } from 'src/app/services/servicio-usuario.service';
+import {Router} from '@angular/router';
 
 
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { from } from 'rxjs';
 import { Usuario } from 'src/app/modelo/Usuario';
+
 
 @Component({
   selector: 'app-registro',
@@ -15,16 +16,16 @@ export class RegistroComponent implements OnInit {
 
   public formregistro: FormGroup;
   public usuarios: Usuario;
-
-  constructor(private formBuilder: FormBuilder, private mimodeloUsuario: ModeloUsuarioService) {
+  
+  constructor(private formBuilder: FormBuilder, private mimodeloUsuario: ModeloUsuarioService, private router: Router) {
 
     // variables para los datos del formulario, se tiene que llamar igual que los formcontroller name
     this.formregistro = formBuilder.group({
-      nick: ['', [Validators.required]],
+      nick: ['', [Validators.required]], // falta validador de espacios
       nombre: ['', [Validators.required]],
       apellidos: ['', [Validators.required]],
       imagen: ['', [Validators.required]],
-      email: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.([a-zA-Z]{2,4})+$/)]],
       password: ['', [Validators.required]],
      // repitepassword: ['', [Validators.required]] // revisar
 
@@ -43,6 +44,7 @@ export class RegistroComponent implements OnInit {
       res => {
         console.log('res de registro.comopontents');
         console.log(res);
+        location.href = ('inicio');
         
       },
       err => {
@@ -54,6 +56,21 @@ export class RegistroComponent implements OnInit {
     );
 
   }
+  // validadcion
+  get nick(){
+    return this.formregistro.get('nick');
+  }
+  
+  get nombre(){
+    return this.formregistro.get('nombre');
+  }
 
+  get apellidos(){
+    return this.formregistro.get('apellidos');
+  }
+
+  get email(){
+    return this.formregistro.get('email');
+  }
 
 }
